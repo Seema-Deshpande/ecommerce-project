9c0edfe24b83f919dc6291a0399b43f462dc79aa
Seema-Deshpande <seemadeshpande12@gmail.com>
2026-01-27 16:58:57 -0600

    feat: add Orders and Tracking pages with corresponding styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 import './App.css'
 import {Routes, Route } from 'react-router'
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
   )
 }
