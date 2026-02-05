6083b02e63352f598b75c6870dbb5a66ce639657
Seema-Deshpande <seemadeshpande12@gmail.com>
2026-02-04 19:49:58 -0600

    Fetched data from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,11 @@ import './HomePage.css';
 import Header from '../components/Header.jsx';
 import { products } from '../data/product-data.js';
 export default function HomePage() {
+    fetch('http://localhost:3000/api/products').then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);  
+    });
     return (
         <>
         <title>Ecommerce Page</title>
