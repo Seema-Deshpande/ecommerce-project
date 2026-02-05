e5d5018abe613b5daeea70d37b1e72f37f0fda62
Seema-Deshpande <seemadeshpande12@gmail.com>
2026-02-04 20:09:15 -0600

    Lift cart state up and share it

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import TrackingPage from './pages/TrackingPage'
 import './App.css'
 import {Routes, Route } from 'react-router'
 import PageNotFound from './pages/PageNotFound.jsx'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 function App() {
+      const [cart, setCart] = useState([]);
+      useEffect(() => {
+              axios.get('/api/cart-items')
+            .then((response) => {
+                setCart(response.data);
+            });
+      },[])
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage  cart={cart}/>} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
       <Route path="*" element={<PageNotFound />} />
