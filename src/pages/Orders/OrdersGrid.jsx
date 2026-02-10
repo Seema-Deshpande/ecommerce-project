9754690c977331f65c0d902b50ec71e9d919a779
Seema-Deshpande <seemadeshpande12@gmail.com>
2026-02-10 15:26:19 -0600

    Seperate code as per component for Orders page

---
+++ src/pages/Orders/OrdersGrid.jsx
@@ -0,0 +1,37 @@
+import dayjs from 'dayjs';
+import { formatMoney } from '../../utils/money';
+import { Fragment } from 'react';
+import { OrderProducts } from './OrdersProducts';
+
+export function OrdersGrid ({ orders }) {
+    return (
+        <>
+         <div className="orders-grid">
+          { orders.map((order) => {
+             return (
+                <div className="order-container" key={order.id}>
+                 <div className="order-header">
+                    <div className="order-header-left-section">
+                        <div className="order-date">
+                            <div className="order-header-label">Order Placed:</div>
+                            <div>{dayjs(order.orderTimeMs).format('MMMM D')}</div>
+                        </div>
+                        <div className="order-total">
+                            <div className="order-header-label">Total:</div>
+                            <div>{formatMoney(order.totalCostCents)}</div>
+                        </div>
+                        </div>
+
+                        <div className="order-header-right-section">
+                            <div className="order-header-label">Order ID:</div>
+                            <div>{order.id}</div>
+                        </div>
+                    </div>
+                    <OrderProducts order={order} />
+                </div>
+                        )
+                    })}
+                </div>
+        </>
+    )
+}
